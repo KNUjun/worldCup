fd091129b0732a7fc4df799b4a9ea8e8b2e06c12
KNUjun <48755258+KNUjun@users.noreply.github.com>
2019-06-30 06:38:15 +0900

    Add files via upload

---
+++ Worldcup.java
@@ -0,0 +1,291 @@
+import java.util.*;
+
+class Worldcup {
+	public static void main(String [] args)throws Exception{
+		
+		Opening();
+		Menu();
+		
+	
+	}
+	
+	static void Opening() throws Exception{
+		
+		System.out.println("¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú                                                                                          ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú     _|          _|  _|_|_|_|  _|          _|_|_|    _|_|    _|      _|  _|_|_|_|  _|     ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú     _|          _|  _|        _|        _|        _|    _|  _|_|  _|_|  _|        _|     ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú     _|    _|    _|  _|_|_|    _|        _|        _|    _|  _|  _|  _|  _|_|_|    _|     ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú       _|  _|  _|    _|        _|        _|        _|    _|  _|      _|  _|               ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú         _|  _|      _|_|_|_|  _|_|_|_|    _|_|_|    _|_|    _|      _|  _|_|_|_|  _|     ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú                                                                                          ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú");	
+	}
+	static void Closing() throws Exception{
+		
+		System.out.println("¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú                                                                                          ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú    _|_|_|    _|_|    _|      _|  _|_|_|_|        _|_|    _|      _|  _|_|_|_|  _|_|_|    ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú  _|        _|    _|  _|_|  _|_|  _|            _|    _|  _|      _|  _|        _|    _|  ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú  _|  _|_|  _|_|_|_|  _|  _|  _|  _|_|_|        _|    _|  _|      _|  _|_|_|    _|_|_|    ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú  _|    _|  _|    _|  _|      _|  _|            _|    _|    _|  _|    _|        _|    _|  ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú    _|_|_|  _|    _|  _|      _|  _|_|_|_|        _|_|        _|      _|_|_|_|  _|    _|  ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú                                                                                          ¡Ú");
+		Thread.sleep(100);
+		System.out.println("¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú");	
+	}
+	static void Table() throws Exception{
+		Thread.sleep(90);
+		System.out.println("|  1.  ÀÌ»óÇü ¿ùµåÄÅ 1¹ø                                                                    |");
+		Thread.sleep(90);
+		System.out.println("|  2. 'ÀÌ»óÇü ¿ùµåÄÅ' ÇÁ·Î±×·¥ Á¾·á!                                                        |");
+		
+	}
+	static void Menu()throws Exception{
+		Scanner s = new Scanner(System.in);
+		Wc w = new Wc();
+		int num;
+		Thread.sleep(500);
+		System.out.println(" ============================================================================================ ");
+		Table();
+		System.out.println(" ============================================================================================ ");
+		Thread.sleep(100);
+		System.out.print("|  ¾î¶² °ÍÀ» ½ÇÇà ÇÏ½Ã°Ù½À´Ï±î?(1,2) ");
+		num = s.nextInt();
+		System.out.println();
+		if(num!=1&&num!=2){
+			for(int i=0 ; i <3 ; i++){
+				System.out.print(" ¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú[ ÀÔ·Â ¿À·ù!!]¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú ");
+				Thread.sleep(100);
+				System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+				Thread.sleep(100);
+				System.out.print(" ¡Ù¡Ù¡Ù¡Ù¡Ù¡Ù[ ÀÔ·Â ¿À·ù!!]¡Ù¡Ù¡Ù¡Ù¡Ù¡Ù ");
+				Thread.sleep(100);
+				System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+				Thread.sleep(100);
+			}
+			System.out.println(" ¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú[ ÀÔ·Â ¿À·ù!!]¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú \n");
+			Menu();
+		}else if(num==1) w.wc_16();
+		else Closing();
+		
+	}
+	
+} 
+
+class Wc{
+	public static ArrayList<Integer> indexes = new ArrayList<>();
+	public static String [] names = {"¶×ÀÌ","¶óÀÌ¾ð","ÅÂº¸ÇØ","¹ÌÅ°¸¶¿ì½º","»Ç·Î·Î","½É½¼","»ó»óµµ ¸øÇÑ Á¤Ã¼","ÈÞ¸Õ?","Á¦¸®","Â¯±¸","±Ã¿¹","»§»§ÀÌ","ÈÞ¸Õ?","µµ¶ó¿¡¸ù","°³±¸¸®","±âÃ¶ÀÌ"};
+	public static int[] index_init = new int[16];
+	public static String[] files = {"1.txt","2.txt","3.txt","4.txt","5.txt","6.txt","7.txt","8.txt","9.txt","10.txt","11.txt","12.txt","13.txt","14.txt","15.txt","16.txt"};
+	Wc(){
+		for(int i=0; i<index_init.length; i++){
+			index_init[i] = i;
+		}
+	}
+	public void wc_16() throws Exception{
+		suffle();
+		int n = 1 ;
+		int index = 0;
+		while( n<=8 ){
+			int i = indexes.get(index);
+			int j = indexes.get(index+1);
+			System.out.println(" -------------------------------------------------------------------------------------------- ");
+			System.out.printf("| ¡º16Round %dth Match¡»                                                                     |\n",n);
+			Thread.sleep(500);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n-1);
+			Thread.sleep(100);
+			print_img(i);
+			System.out.println(names[i]);
+			Thread.sleep(1000);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n);
+			Thread.sleep(100);
+			print_img(j);
+			System.out.println(names[j]);
+			Thread.sleep(500);
+			int win = choice(i, j);
+			if(win == i)
+				indexes.remove(index+1);
+			else
+				indexes.remove(index);
+			n++;
+			index++;
+		}
+		Thread.sleep(200);
+		System.out.println(" ============================================================================================ ");
+		BBuBBa();
+		wc_8();
+	}
+	public void wc_8() throws Exception{
+		int n = 1 ;
+		int index = 0;
+		while( n<=4 ){
+			int i = indexes.get(index);
+			int j = indexes.get(index+1);
+			System.out.println(" ------------------------------------------------------------------------------------------- ");
+			System.out.printf(" ¡º8Round %dth Match¡»                                    \n",n);
+			Thread.sleep(500);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n-1);
+			Thread.sleep(100);
+			print_img(i);
+			System.out.println(names[i]);
+			Thread.sleep(1000);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n);
+			Thread.sleep(100);
+			print_img(j);
+			System.out.println(names[j]);
+			Thread.sleep(500);
+			int win = choice(i, j);
+			if(win == i)
+				indexes.remove(index+1);
+			else
+				indexes.remove(index);
+			n++;
+			index++;
+		}
+		Thread.sleep(200);
+		System.out.println(" ============================================================================================ ");
+		BBuBBa();
+		wc_4();
+	}
+	public void wc_4() throws Exception{
+		int n = 1 ;
+		int index = 0;
+		while( n<=2 ){
+			int i = indexes.get(index);
+			int j = indexes.get(index+1);
+			System.out.println(" -------------------------------------------------------------------------------------------- ");
+			System.out.printf(" ¡ºSemi-Final %dth Match¡»                             \n",n);
+			Thread.sleep(500);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n-1);
+			Thread.sleep(100);
+			print_img(i);
+			System.out.println(names[i]);
+			Thread.sleep(1000);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n);
+			Thread.sleep(100);
+			print_img(j);
+			System.out.println(names[j]);
+			Thread.sleep(500);
+			int win = choice(i, j);
+			if(win == i)
+				indexes.remove(index+1);
+			else
+				indexes.remove(index);
+			n++;
+			index++;
+		}
+		Thread.sleep(200);
+		System.out.println(" ============================================================================================ ");
+		BBuBBa();
+		wc_f();
+	}
+	public void wc_f() throws Exception{
+		int n = 1 ;
+		int index = 0;
+		while( n<=1 ){
+			int i = indexes.get(index);
+			int j = indexes.get(index+1);
+			System.out.println(" ¡ºFINAL MATCH!!¡»                                   ");
+			Thread.sleep(500);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n-1);
+			Thread.sleep(100);
+			print_img(i);
+			System.out.println(names[i]);
+			Thread.sleep(1000);
+			System.out.printf("\n\n|  ¢ÜNUMBER %d¢Ü                                                                             |\n", 2*n);
+			Thread.sleep(100);
+			print_img(j);
+			System.out.println(names[j]);
+			Thread.sleep(500);
+			int win = choice(i, j);
+			if(win == i)
+				indexes.remove(index+1);
+			else
+				indexes.remove(index);
+			n++;
+			index++;
+		}
+		Thread.sleep(200);
+		System.out.println(" ============================================================================================ ");
+		wc_w();
+	}
+	public void wc_w() throws Exception{
+		int i = indexes.get(0);
+		for(int k = 0; k <3 ; k++){
+			System.out.print("  ¡Ú¡Ú¡Ú¡Ú¡Ú¡ÚTHE WINNER¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú ");
+			Thread.sleep(200);
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+			System.out.print("  ¡Ù¡Ù¡Ù¡Ù¡Ù¡ÙTHE WINNER¡Ù¡Ù¡Ù¡Ù¡Ù¡Ù ");
+			Thread.sleep(200);
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");	
+		}
+		System.out.print("  ¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú");
+		System.out.print("¡Ú¡Ú¡Ú¡Ú¡Ú¡ÚTHE WINNER¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú");
+		System.out.println("¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú ");
+		Thread.sleep(500);
+		print_img(i);
+		System.out.println(names[i]);
+	}
+	public int choice(int i, int j) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("|  CHOOSE 1 or 2 : ");
+		int win = sc.nextInt();
+		if( win == 1 )
+			return i;
+		else
+			return j;
+	}
+	
+	public void suffle() throws Exception{
+		//suffling
+		for(int i = 0; i < index_init.length; i++) {
+			int j = (int)(Math.random()*index_init.length);
+			int temp = index_init[i];
+			index_init[i] = index_init[j]; 
+			index_init[j] = temp;
+		}
+		//initializing
+		for(int i = 0; i < index_init.length; i++) {
+			indexes.add(index_init[i]);
+		}
+	}
+	
+	public void BBuBBa() throws Exception{
+		for(int i = 0; i <3 ; i++){
+			System.out.print("  ¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú»Ñ¼õºü¼õ»Ñ¼õºü¼õ¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú ");
+			Thread.sleep(200);
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+			System.out.print("  ¡Ù¡Ù¡Ù¡Ù¡Ù¡Ù»Ñ¼õºü¼õ»Ñ¼õºü¼õ¡Ù¡Ù¡Ù¡Ù¡Ù¡Ù ");
+			Thread.sleep(200);
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");	
+			
+		}
+	}
+	public void print_img(int i) throws Exception{
+		java.io.File file = new java.io.File(files[i]);
+		try(
+			Scanner input = new Scanner(file);
+		){
+			while(input.hasNext()){
+				String line = input.nextLine();
+				System.out.println(line);
+			}
+		}
+	}
+}
